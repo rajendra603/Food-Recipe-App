2d64bbb4ed573ff7d6a265b66c1f025721cd99fd
rajendra603 <bansodrajendra23@gmail.com>
2025-01-28 09:31:41 +0530

    Update Home.js

--- mernapp/src/Screens/Home.js
+++ mernapp/src/Screens/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
   const [foodItem, setfoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/foodData", {
+    let response = await fetch("https://food-app-00un.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
